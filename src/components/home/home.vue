7b55e924d9efc04d172f5d158f8f981b380e4a30
XueCong <xuecong98@gmail.com>
2018-05-07 19:46:30 +0800

    change swiper

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <v-header></v-header>
+    <header-swiper></header-swiper>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
 import Header from './components/header'
+import HeaderSwiper from './components/HeaderSwiper'
 
 export default {
   name: 'Home',
   components: {
-    'v-header': Header
+    'v-header': Header,
+    HeaderSwiper
   }
 }
 </script>
