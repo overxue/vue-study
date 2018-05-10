b5ffa0baa12de91f187d6f5525a6af0dcd8b2b3b
XueCong <xuecong98@gmail.com>
2018-05-10 21:44:27 +0800

    city router

---
+++ src/components/city/city.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <v-header></v-header>
+    </div>
+</template>
+
+<script>
+import Header from './components/header'
+
+export default {
+  name: 'City',
+  components: {
+    'v-header': Header
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+
+</style>
