21977b2bbbe40e56c1bfd6f1ca9449af65efd12a
XueCong <xuecong98@gmail.com>
2018-05-16 21:05:43 +0800

    Header animation

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -1,20 +1,25 @@
 <template>
     <div>
       <banner></banner>
+      <v-header></v-header>
+      <div class="content"></div>
     </div>
 </template>
 
 <script>
 import Banner from './components/banner'
+import vHeader from './components/vheader'
 
 export default {
   name: 'Detail',
   components: {
-    Banner
+    Banner,
+    vHeader
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  .content
+    height: 50rem
 </style>
