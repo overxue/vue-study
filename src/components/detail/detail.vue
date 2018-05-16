3b82c45f9f1919e4fe3176512b139a37fe7242d1
XueCong <xuecong98@gmail.com>
2018-05-16 22:03:57 +0800

    Edit bug

--- src/components/detail/detail.vue
+++ src/components/detail/detail.vue
@@ -2,19 +2,44 @@
     <div>
       <banner></banner>
       <v-header></v-header>
-      <div class="content"></div>
+      <div class="content">
+        <list :list="list"></list>
+      </div>
     </div>
 </template>
 
 <script>
 import Banner from './components/banner'
 import vHeader from './components/vheader'
+import List from './components/list'
 
 export default {
   name: 'Detail',
   components: {
     Banner,
-    vHeader
+    vHeader,
+    List
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          'title': '成人三馆联票',
+          children: [{
+            'title': '三级'
+          }]
+        }, {
+          'title': '成人五馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
