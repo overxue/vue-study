c305c14d13c23ad6a2dfe234fae14a81fbccd754
XueCong <xuecong98@gmail.com>
2018-05-21 22:40:49 +0800

    Add better-scroll

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,12 +1,59 @@
 <template>
   <div class="recommend">
-    recommend
+    <div class="recommend-content">
+      <div v-if="recommends.length" class="slider-wrapper">
+        <slider>
+          <div v-for="item of recommends" :key="item.id">
+            <a :href="item.linkUrl">
+              <img :src="item.picUrl">
+            </a>
+          </div>
+        </slider>
+      </div>
+      <div class="recommend-list">
+        <h1 class="list-title">热门歌单推荐</h1>
+        <ul></ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
-export default {}
+import Slider from 'base/slider/slider'
+import {getRecommend} from 'api/recommend'
+import {ERR_OK} from 'api/config'
+
+export default {
+  data () {
+    return {
+      recommends: []
+    }
+  },
+  created () {
+    this._getRecommend()
+  },
+  methods: {
+    _getRecommend () {
+      getRecommend().then((res) => {
+        if (res.code === ERR_OK) {
+          this.recommends = res.data.slider
+        }
+      })
+    }
+  },
+  components: {
+    Slider
+  }
+}
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+  .recommend
+    /*position: fixed*/
+    /*width: 100%*/
+    /*top: 88px*/
+    .recommend-content
+      .slider-wrapper
+        width: 100%
 </style>
