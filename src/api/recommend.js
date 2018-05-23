1ba33d4c6c6893b15ddfaa0476e60eb174988f24
XueCong <xuecong98@gmail.com>
2018-05-23 22:33:26 +0800

    Add song list

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueJsonp from 'vue-jsonp'
 import {commonParams, options} from './config'
+import axios from 'axios'
 Vue.use(VueJsonp)
 
 export function getRecommend () {
@@ -12,4 +13,26 @@ export function getRecommend () {
     needNewCode: 1
   })
   return Vue.jsonp(url, data)
+}
+
+export function getDiscList () {
+  const url = '/api/getDiscList'
+
+  const data = Object.assign({}, commonParams, {
+    platform: 'yqq',
+    hostUin: 0,
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000,
+    rnd: Math.random(),
+    format: 'json'
+  })
+
+  return axios.get(url, {
+    params: data
+  }).then((res) => {
+    return Promise.resolve(res.data)
+  })
 }
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -12,7 +12,17 @@
       </div>
       <div class="recommend-list">
         <h1 class="list-title">热门歌单推荐</h1>
-        <ul></ul>
+        <ul>
+          <li v-for="(item, index) of discList" class="item" :key="index">
+            <div class="icon">
+              <img width="60" height="60" :src="item.imgurl">
+            </div>
+            <div class="text">
+              <h2 class="name" v-html="item.creator.name"></h2>
+              <p class="desc" v-html="item.dissname"></p>
+            </div>
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -20,23 +30,32 @@
 
 <script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
-import {getRecommend} from 'api/recommend'
+import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
   data () {
     return {
-      recommends: []
+      recommends: [],
+      discList: []
     }
   },
   created () {
     this._getRecommend()
+    this._getDiscList()
   },
   methods: {
     _getRecommend () {
       getRecommend().then((res) => {
         if (res.code === ERR_OK) {
           this.recommends = res.data.slider
+        }
+      })
+    },
+    _getDiscList () {
+      getDiscList().then((res) => {
+        if (res.code === ERR_OK) {
+          this.discList = res.data.list
         }
       })
     }
@@ -54,7 +73,36 @@ export default {
     /*width: 100%*/
     /*top: 88px*/
     .recommend-content
+      overflow: hidden
       .slider-wrapper
         position: relative
         width: 100%
+    .recommend-list
+      .list-title
+        height: 65px
+        line-height: 65px
+        text-align: center
+        font-size: $font-size-medium
+        color: $color-theme
+      .item
+        display: flex
+        align-items: center
+        padding: 0 20px 20px 20px
+        .icon
+          flex: 0 0 60px
+          width: 60px
+          padding-right: 20px
+        .text
+          display: flex
+          flex-direction: column
+          justify-content: center
+          flex: 1
+          line-height: 20px
+          overflow: hidden
+          font-size: $font-size-medium
+          .name
+            margin-bottom: 10px
+            color: $color-text
+          .desc
+            color: $color-text-d
 </style>
